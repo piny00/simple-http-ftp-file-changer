b8c7f1a3a1e4c481e97c8fb4ef01051917d50bd4
piny00 <tosoki.laszlo@fotex.net>
2025-08-26 23:52:56 +0200

    nginx permission fix

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-chmod -R 777 /usr/share/nginx/html/uploads
-exec nginx -g "daemon off;"
-
+nginx -g "daemon off;" &
+while true; do
+    inotifywait -e create -r /usr/share/nginx/html/uploads && \
+    find /usr/share/nginx/html/uploads -type f ! -perm 0777 -exec chmod 777 {} \; && \
+    find /usr/share/nginx/html/uploads -type d ! -perm 0777 -exec chmod 777 {} \;
+done
